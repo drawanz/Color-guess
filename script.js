c592407b55d94411c4bb873c3fa552887fe50f36
Leonidas <leonidas.drawanz@gmail.com>
2021-12-23 12:04:20 -0300

    projeto finalizado - todos requisitos ok

--- script.js
+++ script.js
@@ -2,15 +2,8 @@ let containerCores = document.querySelector('#container-cores');
 let arrayCores = [];
 let arrayAdivinhacao = [];
 let textoRgb = document.getElementById('rgb-color');
-//let pontuacao =  sessionStorage.getItem('valor');
+let pontuacao = 0;
 let placar = document.getElementById('score');
-if (sessionStorage.getItem('valor') === null) {
-    pontuacao = 0;
-    console.log(pontuacao);
-} else {
-    pontuacao = JSON.parse(sessionStorage.getItem('valor'));
-    console.log(pontuacao);
-}
 placar.innerText = "Placar: " + pontuacao + " pontos";
 let acertouErrou = document.getElementById('answer');
 let botao = document.getElementById('reset-game');
@@ -47,7 +40,7 @@ function procurandoResposta(event){
         acertouErrou.innerText = "Acertou!"
         pontuacao = pontuacao + 3;
         placar.innerText = "Placar: " + pontuacao + " pontos"
-        sessionStorage.setItem('valor', pontuacao)
+       // sessionStorage.setItem('valor', pontuacao)
     }    
     else {
         acertouErrou.innerText = "Errou! Tente novamente!";
@@ -66,8 +59,38 @@ function criandoEscutadores(){
 }
 
 function resetarJogo(){
-    location.reload()
-    //nppontuacao = sessionStorage.getItem('valor');
+    let arrayCores2 = [];
+    let arrayAdivinhacao2 = [];
+
+    while (containerCores.firstChild){
+        containerCores.removeChild(containerCores.firstChild)
+    }
+
+    for (let i = 0; i < 6; i+=1){
+        let r = Math.floor(Math.random()*255);
+        let g = Math.floor(Math.random()*255);
+        let b = Math.floor(Math.random()*255);
+        let rgb = "rgb(" + r + ', ' + g + ', ' + b + ")";
+        arrayCores2.push(rgb);
+        let adivinhacao = "(" + r + ', ' + g + ', ' + b + ")";
+        arrayAdivinhacao2.push(adivinhacao)
+    }  
+
+    let posicaoAleatoria = Math.floor(Math.random()*5)
+    textoRgb.innerText = arrayAdivinhacao2[posicaoAleatoria]
+
+    for (let i = 0; i < 6; i +=1){
+        let divCores = document.createElement('div');
+        containerCores.appendChild(divCores);
+        divCores.className = 'ball';  
+        divCores.style.backgroundColor = arrayCores2[i];
+    }
+    criandoEscutadores();
+
+    acertouErrou.innerText = "Escolha uma cor"
+    console.log(arrayCores2);
+    console.log(arrayAdivinhacao2);
+
 }
 
 criandoCoresAleatorias();
